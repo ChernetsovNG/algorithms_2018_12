a2d577a91f540c51a4a194b880314bf006e93373
ChernetsovNG <n.chernetsov86@gmail.com>
2019-03-24 12:48:15 +0300

    HW16: init commit

---
+++ otus-algorithms-HW/HW16/src/main/java/ru/nchernetsov/DijkstraAlg.java
@@ -0,0 +1,20 @@
+package ru.nchernetsov;
+
+import ru.nchernetsov.graph.Edge;
+import ru.nchernetsov.graph.WeightedDirectedGraph;
+
+/**
+ * Алгоритм Дейкстры
+ */
+public class DijkstraAlg {
+
+    /**
+     * Алгоритм Дейкстры поиска кратчайшего пути во взвешенном ориентированном графе
+     *
+     * @param graph взвешенный ориентированный граф
+     * @return массив рёбер, составляющих кратчайший путь
+     */
+    public static Edge[] dijkstraAlg(WeightedDirectedGraph graph) {
+        return new Edge[10];
+    }
+}
