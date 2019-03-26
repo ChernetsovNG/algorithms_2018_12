8f96aeecd1d91f0aa5f588617f64463cf0c3033a
ChernetsovNG <n.chernetsov86@gmail.com>
2019-03-26 14:58:39 +0300

    HW17: добавлена реализация алгоритма A* на плоском графе (с 2D вершинами)

--- otus-algorithms-HW/HW17/src/main/java/ru/nchernetsov/AStarAlg.java
+++ otus-algorithms-HW/HW17/src/main/java/ru/nchernetsov/AStarAlg.java
@@ -1,7 +1,57 @@
 package ru.nchernetsov;
 
+import ru.nchernetsov.graph.VertexPoint2D;
+import ru.nchernetsov.graph.WeightedDirectedGraph2D;
+import ru.nchernetsov.heap.MinHeap;
+import ru.nchernetsov.tuples.Pair;
+
 /**
  * Алгоритм A* поиска маршрута с наименьшей стоимостью между двумя вершинами в графе
  */
 public class AStarAlg {
+
+    /**
+     * Алгоритм Дейкстры поиска кратчайшего пути во взвешенном ориентированном графе
+     *
+     * @param graph      взвешенный ориентированный граф
+     * @param vertexFrom начальная вершина
+     * @param vertexTo   конечная вершина
+     * @return массив вершин, составляющих кратчайший путь из начальной вершины в конечную
+     */
+    public static Path aStarAlg(WeightedDirectedGraph2D graph, int vertexFrom, int vertexTo) {
+        int edgesCount = graph.getEdgesCount();
+        VertexPoint2D[] vertices = graph.getVertices();
+        MyArrayList<MyArrayList<Pair<Integer, Double>>> adjVectorsList = graph.getAdjVectorsList();
+
+        VertexPoint2D start = vertices[vertexFrom];
+        VertexPoint2D goal = vertices[vertexTo];
+
+        // Множество частных решений
+        MinHeap<Path> open = new MinHeap<>(edgesCount);
+
+        open.add(new Path(start, goal));
+        while (open.isNotEmpty()) {
+            Path p = open.remove();
+            VertexPoint2D x = p.lastVertex();
+            if (x.isVisited()) {
+                continue;
+            }
+            if (x.getIndex() == vertexTo) {
+                return p;
+            }
+            x.setVisited(true);
+            // добавляем смежные вершины
+            MyArrayList<Pair<Integer, Double>> adjVector = adjVectorsList.get(x.getIndex());
+            for (Pair<Integer, Double> adjVertexAndEdgeWeight : adjVector) {
+                Integer adjVertexIndex = adjVertexAndEdgeWeight.getFirst();
+                VertexPoint2D adjVertex = vertices[adjVertexIndex];
+                Path newPath = new Path(p);
+                newPath.addVertex(adjVertex);
+                open.add(newPath);
+            }
+        }
+
+        // if path not found => return failure empty path
+        return new Path(vertices[vertexTo]);
+    }
 }
---
+++ otus-algorithms-HW/HW17/src/test/java/ru/nchernetsov/AStarAlgTest.java
@@ -0,0 +1,49 @@
+package ru.nchernetsov;
+
+import org.junit.Test;
+import ru.nchernetsov.graph.VertexPoint2D;
+import ru.nchernetsov.graph.WeightedDirectedGraph2D;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.nchernetsov.AStarAlg.aStarAlg;
+
+public class AStarAlgTest {
+
+    @Test
+    public void aStarAlgTest1() {
+        WeightedDirectedGraph2D graph = new WeightedDirectedGraph2D(8, 11);
+
+        graph.addVertex(0, 0.0, 0.0);
+        graph.addVertex(1, 1.0, 3.0);
+        graph.addVertex(2, 1.0, 6.0);
+        graph.addVertex(3, 3.0, 7.0);
+        graph.addVertex(4, 4.0, 6.0);
+        graph.addVertex(5, 3.0, 1.0);
+        graph.addVertex(6, 5.0, 0.0);
+        graph.addVertex(7, 6.0, 4.0);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 5);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+        graph.addEdge(5, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(6, 7);
+        graph.addEdge(7, 4);
+        graph.addEdge(6, 4);
+
+        Path pathFrom0to7 = aStarAlg(graph, 0, 7);
+        MyArrayList<VertexPoint2D> pathFrom0to7vertices = pathFrom0to7.getVertices();
+        List<Integer> pathFrom0to7verticesIndices = pathFrom0to7vertices.stream()
+            .map(VertexPoint2D::getIndex)
+            .collect(Collectors.toList());
+
+        assertThat(pathFrom0to7verticesIndices).hasSize(3);
+        assertThat(pathFrom0to7verticesIndices).containsExactly(0, 5, 7);
+    }
+}
